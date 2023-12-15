ea516aca562c380ca7b382e0f6dc0588ec5afdfb
Out-Flow <153929584+Out-Flow@users.noreply.github.com>
2023-12-15 12:40:28 -0500

    Add files via upload

---
+++ Site_web/Root/Script.js
@@ -0,0 +1,6 @@
+// script.js
+function performSearch() {
+    var searchTerm = document.getElementById('search').value;
+    alert('You searched for: ' + searchTerm);
+    // Vous pouvez remplacer cette alerte par votre propre logique de recherche ou redirection
+}
