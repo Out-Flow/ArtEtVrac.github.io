4786f3de1710e8f08853ac4bcc986420dd4b8221
Out-Flow <153929584+Out-Flow@users.noreply.github.com>
2023-12-17 10:04:50 -0500

    Add files via upload

---
+++ Root/Script.js
@@ -0,0 +1,21 @@
+// script.js
+function performSearch() {
+    var searchTerm = document.getElementById('search').value;
+    alert('nothing found for: ' + searchTerm);
+    // Vous pouvez remplacer cette alerte par votre propre logique de recherche ou redirection
+}
+
+
+
+function shareOnFacebook() {
+    window.open('https://www.facebook.com/sharer/sharer.php?u=https://file:///C:/Users/Ander/OneDrive/Documents/Ludovic/Site_web/Root/projects.html', '_blank');
+}
+
+function shareOnTwitter() {
+    window.open('https://twitter.com/intent/tweet?url=https://file:///C:/Users/Ander/OneDrive/Documents/Ludovic/Site_web/Root/projects.html&text=Check out this amazing portfolio!', '_blank');
+}
+
+function shareOnInstagram() {
+    // Instagram doesn't allow direct sharing via URL, so you can provide a link to your Instagram profile.
+    window.open('https://www.instagram.com/https://www.instagram.com/outflow.music//', '_blank');
+}
